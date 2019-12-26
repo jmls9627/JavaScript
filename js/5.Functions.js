de05c4dbcd67b4e7ad605138ee80d7cb657a3e07
jmls9627 <jmls9627@gmail.com>
2019-12-26 15:20:14 -0500

    events

--- js/5.Functions.js
+++ js/5.Functions.js
@@ -35,3 +35,65 @@ console.log(result);
      console.log("demas ingredientes",masingr);
  }
  cocinar("pollo","tomate","malanga","arroz","agua");
+
+  //***************Parametros spread****************
+
+  function cocinar1(ingre1, ingre2, ...masingre){
+    console.log("ingrediente1 ",ingre1 );
+    console.log("ingrediente2 ",ingre2);
+    console.log("demas ingredientes",masingre);
+}
+
+var ingredientes=["pollo","tomate"];
+cocinar(...ingredientes,"malanga","arroz","agua");
+
+//***************FUNCIONES ANONiMAS  ****************
+(
+    function(){
+        var msj="hola";
+        console.log(msj);
+    }
+)()
+
+var sal=function(nombre){
+    var msj="hello ", nombre;
+    return msj;
+}
+
+//***************callbacks  ****************
+
+function calcular(x,y,sumacb,restacb){
+    var suma=x+y;
+    var resta=x-y;
+
+    sumacb(suma);
+    restacb(resta);
+}
+
+calcular(3,2,function(result){
+console.log("suma",result)
+},function(result){
+    console.log("resta",result)
+})
+
+//***************funcion arrow|fat arrow|lambda ****************
+
+var saludar2=nombre =>"hola"+nombre;
+console.log(saludar2("susana"));
+
+var sumar=cant =>cant+10
+console.log(sumar(10));
+
+var calc=(a,b) =>a+b
+console.log(calc(3,4));
+
+var generar=(a,b) =>{
+    var c=5;
+    return a+b+c;
+}
+console.log(generar(10,15));
+
+var validar= () =>{
+    return "validacion correcta";
+}
+console.log(validar());
---
+++ js/6.Events.js
@@ -0,0 +1,54 @@
+//*********************EVENTOS DEL MOUSE************* */
+const boton=document.querySelector(".boton");
+
+boton.addEventListener('click',function(){
+    console.log("se ha pulsado");
+})
+
+boton.addEventListener('mouseover',function(){
+    console.log("el mouse esta sobre el boton");
+})
+
+boton.addEventListener('mouseout',function(){
+    console.log("el mouse esta fuera del boton");
+})
+
+//*********************EVENTOS DEL TECLADO************* */
+
+window.addEventListener('keydown',function(event){
+    console.log("pulsando tecla");
+    console.log(event.keyCode);
+    console.log(String.fromCharCode(event.keyCode))
+})
+
+window.addEventListener('keypress',function(event){
+    console.log("tecla pulsada");
+})
+
+window.addEventListener('keyup',function(event){
+    console.log("tecla liberada");
+})
+
+//*********************Carga de documentos************* */
+
+window.addEventListener('load',function(){
+    console.log("el contenido de la ventana se ha cargado");
+})
+
+//*********************EVENTOS MULTIMEDIA************* */
+
+const video=document.querySelector(".bostonVideo");
+
+video.addEventListener('play',function(){
+    console.log("se ha iniciado");
+});
+
+video.addEventListener('seeking',function(){
+    console.log("se esta buscando en el video", this.currentTime);
+});
+
+video.addEventListener('ended',function(){
+    console.log("el video ha terminado");
+});
+
+//*********************EVENTOS MULTIMEDIA************* */
